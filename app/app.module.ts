fcdcf87166e8979e2c1965d810612fef2a2ee7b2
bayat <turtbayev@gmail.com>
2017-08-20 21:45:56 +0600

    :using http

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './shared/data.service';
+
 import { AppComponent } from './app.component';
 import { TodoFormComponent } from './todo-form/todo-form.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
@@ -8,7 +13,12 @@ import { TodoItemComponent } from './todo-item/todo-item.component';
 import { TodoService } from './shared/todo.service';
 
 @NgModule({
-	imports: [BrowserModule, FormsModule],
+	imports: [
+		BrowserModule, 
+		FormsModule, 
+		HttpModule,
+		InMemoryWebApiModule.forRoot(InMemoryDataService)
+	],
 	declarations: [
 		AppComponent, 
 		TodoFormComponent,
